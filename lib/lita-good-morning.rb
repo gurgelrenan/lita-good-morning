ba81569ac6292741eba0d7ece6c67f981e0b3a16
Renan Gurgel <gurgel.renan@gmail.com>
2015-10-29 18:26:30 -0300

    add code for good morning

--- lib/lita-good-morning.rb
+++ lib/lita-good-morning.rb
@@ -3,7 +3,8 @@ require "lita"
 Lita.load_locales Dir[File.expand_path(
   File.join("..", "..", "locales", "*.yml"), __FILE__
 )]
-
+require 'active_support'
+require 'active_support/core_ext'
 require "lita/handlers/good_morning"
 
 Lita::Handlers::GoodMorning.template_root File.expand_path(
--- lib/lita/handlers/good_morning.rb
+++ lib/lita/handlers/good_morning.rb
@@ -1,11 +1,58 @@
 module Lita
   module Handlers
     class GoodMorning < Handler
-      route(/^echo\s+(.+)/, :echo, help: { "echo TEXT" => "Retorna de volta TEXT." })
+      MESSAGES = [
+          'Bom dia galera!',
+          'Goodie Goodie Morning pessoal!',
+          'Que esse dia seja produtivo e delicioso! Bom dia amigos!',
+          'Bom dia.. e alegria.. vamos sorrir e cantar! Dia galera!',
+          'DIAAAAAAAAA',
+          'Bão dia fi!',
+          'Bom Bom Bom Bom diaaaaaaaaaa!',
+          'Woof Woof! Bom dia!',
+          'Good morning america!',
+          'A quem chega, bom dia! Let\'s get to work!',
+          'Let\'s do it to it pessoal! Bom Dia!',
+          'Bundinha! Quer dizer.. Bom dia!',
+          'Dia rapeizee!',
+          'Dia dia dia diiiiaaa!',
+          'Mais um dia começando, que esse seja ótimo galera!']
 
-      def echo(response)
-        response.reply(response.matches)
-      end
+        on :connected, :check_if_its_hello_time
+
+        route(/channel id/, :channel_id, command: true, help: 'Retorna o ID do canal')
+
+        def initialize(*args)
+          log.info 'Handler de bom dia inicializado!'
+          @room = Lita::Source.new(room: 'C03HUG8SG') # General
+          super
+        end
+
+        def channel_id(response)
+          response.reply_with_mention response.message.source.room
+        end
+
+        def check_if_its_hello_time(*_)
+          every(50) do |_|
+            Time.zone = 'Brasilia' # Needed since this will run in another thread..
+
+            # Todo dia de semana as 8 da manha...
+            send_good_morning_message #if Time.zone.now.strftime('%H:%M') == '08:00' &&
+                                      #   ![0, 6].include?(Time.zone.now.wday)
+          end
+        end
+
+        private
+
+        def send_good_morning_message
+          # Only one good morning per day :)
+          return if redis.get 'gave_good_morning'
+
+          robot.send_message(@room, MESSAGES.sample)
+
+          redis.set 'gave_good_morning', true
+          redis.expire 'gave_good_morning', 60
+        end
     end
 
     Lita.register_handler(GoodMorning)
