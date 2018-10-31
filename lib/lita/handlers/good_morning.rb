db921bb84f8de16166fd95f1540bd1b7c0d5c43f
Caio Ergos <caioergos@gmail.com>
2018-10-30 23:32:23 -0300

    fix typos

--- lib/lita/handlers/good_morning.rb
+++ lib/lita/handlers/good_morning.rb
@@ -9,19 +9,19 @@ module Lita
         'Bom dia galera!',
         'Goodie Goodie Morning pessoal!',
         'Que esse dia seja produtivo e delicioso! Bom dia amigos!',
-        'Bom dia.. e alegria.. vamos sorrir e cantar! Dia galera!',
+        'Bom dia... e alegria... vamos sorrir e cantar! Dia galera!',
         'DIAAAAAAAAA',
         'Bão dia fi!',
         'Bom Bom Bom Bom diaaaaaaaaaa!',
         'Woof Woof! Bom dia!',
         'Good morning america!',
         "A quem chega, bom dia! Let's get to work!",
-        "Let's do it to it pessoal! Bom Dia!",
-        'Bundinha! Quer dizer.. Bom dia!',
+        "Let's do it to it, pessoal! Bom Dia!",
+        'Bundinha! Quer dizer... Bom dia!',
         'Dia rapeizee!',
         'Dia dia dia diiiiaaa!',
         'Mais um dia começando, que esse seja ótimo galera!',
-        'Vai dar certo mah!',
+        'Vai dar certo, mah!',
         'Bom dia, rapaziada!'
       ].freeze
 
